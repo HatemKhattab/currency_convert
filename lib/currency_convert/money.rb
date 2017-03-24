a3569a537811a1b89c1642baf1c63a3954bd109d
Hatem Khattab <hattemkhattab@gmail.com>
2017-03-24 09:56:07 +0100

    edit test file

--- lib/currency_convert/money.rb
+++ lib/currency_convert/money.rb
@@ -1,4 +1,5 @@
 class Money
+
   attr_accessor :amount, :currency
 
   def initialize(amount, currency)
@@ -10,4 +11,15 @@ class Money
    amount_f = "%0.2f" % @amount
    return "#{amount_f} #{currency}"
   end
+
+  def self.conversion_rates(from,hash={})
+  end
+
 end
+
+# Configure the currency rates with respect to a base currency (here EUR):
+ 
+# Money.conversion_rates('EUR', {
+#   'USD'     => 1.11,
+#   'Bitcoin' => 0.0047
+# })
--- spec/currency_convert_spec.rb
+++ spec/currency_convert_spec.rb
@@ -1,19 +1,32 @@
 require "spec_helper"
 
 describe CurrencyConvert do
-  it "has a version number" do
-    expect(CurrencyConvert::VERSION).not_to be nil
-  end
+
+  before{
+    Money.conversion_rates('EUR', {
+    'USD'     => 1.11,
+    'Bitcoin' => 0.0047
+    })
+
+    fify_eur = Money.new(50, 'EUR')
+  }
+
 
   it "can create a new money" do
-    fifty_euros = Money.new(50, 'EUR')
-    expect(fifty_euros).to_not be_nil
+    # fifty_eur = Money.new(50, 'EUR')
+    # expect(fifty_eur).to_not be_nil
   end
 
   it "can get amount, currency and inspect" do
-  	fifty_euros = Money.new(50, 'EUR')
-  	expect(fifty_euros.amount).to eq(50)
-  	expect(fifty_euros.currency).to eq('EUR')
-  	expect(fifty_euros.inspect).to eq('50.00 EUR')
+  	expect(fifty_euro.amount).to eq(50)
+  	expect(fifty_euro.currency).to eq('EUR')
+  	expect(fifty_euro.inspect).to eq('50.00 EUR')
+  end
+
+  it "can configure the money" do
+  
+    # fifty_euros = Money.new(50, 'EUR')
+    #expect(Money.from).to eq('EUR')
   end
 end
+
